8eb9c605eb06efe7e16f079acf2205e7733e4b0e
Rchi Lugtu <63701602+rchilugtu@users.noreply.github.com>
2021-01-24 12:44:17 +1300

    initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// adding a click event to the card-container to open pop up card
+const projectCards = document.querySelectorAll('.card-container');
+for (let card of projectCards) {
+    card.addEventListener('click', function() {
+        card.nextElementSibling.classList.add('active');
+    })
+}
+
+// Adding an a click event to the 'x' button of pop up card
+const popupCloseButtons = document.querySelectorAll('.popup-x-btn');
+for (let button of popupCloseButtons) {
+    button.addEventListener('click', function() {
+        button.parentElement.parentElement.offsetParent.classList.remove('active');      
+    })
+}
+
+
+function toggleSidebar() {
+    let sidebar = document.querySelector('#sidebar');
+    sidebar.classList.toggle('active');
+    if(!document.getElementById("overlay").style.display || document.getElementById("overlay").style.display === 'none') {
+        document.getElementById("overlay").style.display = "block"
+    } else {
+        document.getElementById("overlay").style.display = 'none';
+    }
+}
+function toggleSidebarBtn() {
+    let sidebar = document.querySelector('#sidebar');
+    sidebar.classList.toggle('active');
+    document.getElementById("overlay").style.display = "none";
+}
+
+// Overlay Effect
+function on() {
+}
+function off() {
+    let sidebar = document.querySelector('#sidebar');
+    sidebar.classList.toggle('active');
+    document.getElementById("overlay").style.display = "none";
+}
+
+// Card Pop up effect
+function openCardPopUp() {
+    console.log(this);
+    let card = document.querySelector('.card');
+    document.querySelector('.card').lastElementChild.style.top = '0';
+    document.querySelector('.card').lastElementChild.style.cursor = 'auto';
+}
+
+function closeCardPopUp() {
+    let card = document.querySelector('.card');
+    document.querySelector('.card').lastElementChild.style.top = '400px'
+}
+
+
+// Smooth Scrolling
+const scroll = new SmoothScroll('#sidebar a[href*="#"], #navbar a[href*="#"]', {
+    speed: 500
+});
+
