27c3cfe23ef417591c2277889b0432b0c903ed4a
Rchi Lugtu <lugturchi@gmail.com>
2021-01-26 17:39:32 +1300

    added experience section, and fixed pixel bugs

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ const scroll = new SmoothScroll('#sidebar a[href*="#"], #navbar a[href*="#"]', {
     speed: 500
 });
 
+
+// Intro H1 Parallax
 let introText = document.querySelector('#intro-text');
 window.addEventListener('scroll', function() {
     const value = window.scrollY;
