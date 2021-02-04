fdbf26f04b2d18fc9356d5533e5b90a911984f67
Rchi Lugtu <lugturchi@gmail.com>
2021-02-05 10:36:17 +1300

    minor changes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // List of sentences
-var _CONTENT = [ 
-	"Hi, I'm Rchi Lugtu.", 
-	"I'm a Software Developer.", 
-    "I'm an Innovator.",
+var _CONTENT = [
+    "Hi, I'm Rchi Lugtu.",
+    "I'm a Software Engineer.",
+    "I like to create things.",
 ];
 
 // Current sentence being processed
 var _PART = 0;
 
-// Character number of the current sentence being processed 
+// Character number of the current sentence being processed
 var _PART_INDEX = 0;
 
 // Holds the handle returned from setInterval
@@ -17,115 +17,114 @@ var _INTERVAL_VAL;
 // Element that holds the text
 var _ELEMENT = document.querySelector("#intro-text");
 
-// Cursor element 
+// Cursor element
 var _CURSOR = document.querySelector("#cursor");
 
 // Implements typing effect
-function Type() { 
-	// Get substring with 1 characater added
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX++;
+function Type() {
+    // Get substring with 1 characater added
+    var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+    _ELEMENT.innerHTML = text;
+    _PART_INDEX++;
 
-	// If full sentence has been displayed then start to delete the sentence after some time
-	if(text === _CONTENT[_PART]) {
-		// Hide the cursor
-		_CURSOR.style.display = 'none';
+    // If full sentence has been displayed then start to delete the sentence after some time
+    if (text === _CONTENT[_PART]) {
+        // Hide the cursor
+        _CURSOR.style.display = "none";
 
-		clearInterval(_INTERVAL_VAL);
-		setTimeout(function() {
-			_INTERVAL_VAL = setInterval(Delete, 50);
-		}, 1000);
-	}
+        clearInterval(_INTERVAL_VAL);
+        setTimeout(function () {
+            _INTERVAL_VAL = setInterval(Delete, 50);
+        }, 1000);
+    }
 }
 
 // Implements deleting effect
 function Delete() {
-	// Get substring with 1 characater deleted
-	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
-	_ELEMENT.innerHTML = text;
-	_PART_INDEX--;
+    // Get substring with 1 characater deleted
+    var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+    _ELEMENT.innerHTML = text;
+    _PART_INDEX--;
 
-	// If sentence has been deleted then start to display the next sentence
-	if(text === '') {
-		clearInterval(_INTERVAL_VAL);
+    // If sentence has been deleted then start to display the next sentence
+    if (text === "") {
+        clearInterval(_INTERVAL_VAL);
 
-		// If current sentence was last then display the first one, else move to the next
-		if(_PART == (_CONTENT.length - 1))
-			_PART = 0;
-		else
-			_PART++;
-		
-		_PART_INDEX = 0;
+        // If current sentence was last then display the first one, else move to the next
+        if (_PART == _CONTENT.length - 1) _PART = 0;
+        else _PART++;
 
-		// Start to display the next sentence after some time
-		setTimeout(function() {
-			_CURSOR.style.display = 'inline-block';
-			_INTERVAL_VAL = setInterval(Type, 100);
-		}, 200);
-	}
+        _PART_INDEX = 0;
+
+        // Start to display the next sentence after some time
+        setTimeout(function () {
+            _CURSOR.style.display = "inline-block";
+            _INTERVAL_VAL = setInterval(Type, 100);
+        }, 200);
+    }
 }
 
 // Start the typing effect on load
 _INTERVAL_VAL = setInterval(Type, 100);
 
-
 // adding a click event to the card-container to open pop up card
-const projectCards = document.querySelectorAll('.card-container');
+const projectCards = document.querySelectorAll(".card-container");
 for (let card of projectCards) {
-    card.addEventListener('click', function() {
-        card.nextElementSibling.classList.add('active');
-    })
+    card.addEventListener("click", function () {
+        card.nextElementSibling.classList.add("active");
+    });
 }
 
 // Adding an a click event to the 'x' button of pop up card
-const popupCloseButtons = document.querySelectorAll('.popup-x-btn');
+const popupCloseButtons = document.querySelectorAll(".popup-x-btn");
 for (let button of popupCloseButtons) {
-    button.addEventListener('click', function() {
-        button.parentElement.parentElement.offsetParent.classList.remove('active');      
-    })
+    button.addEventListener("click", function () {
+        button.parentElement.parentElement.offsetParent.classList.remove(
+            "active"
+        );
+    });
 }
 
-
 function toggleSidebar() {
-    let sidebar = document.querySelector('#sidebar');
-    sidebar.classList.toggle('active');
-    if(!document.getElementById("overlay").style.display || document.getElementById("overlay").style.display === 'none') {
-        document.getElementById("overlay").style.display = "block"
+    let sidebar = document.querySelector("#sidebar");
+    sidebar.classList.toggle("active");
+    if (
+        !document.getElementById("overlay").style.display ||
+        document.getElementById("overlay").style.display === "none"
+    ) {
+        document.getElementById("overlay").style.display = "block";
     } else {
-        document.getElementById("overlay").style.display = 'none';
+        document.getElementById("overlay").style.display = "none";
     }
 }
 function toggleSidebarBtn() {
-    let sidebar = document.querySelector('#sidebar');
-    sidebar.classList.toggle('active');
+    let sidebar = document.querySelector("#sidebar");
+    sidebar.classList.toggle("active");
     document.getElementById("overlay").style.display = "none";
 }
 
 // Overlay Effect
-function on() {
-}
+function on() {}
 function off() {
-    let sidebar = document.querySelector('#sidebar');
-    sidebar.classList.toggle('active');
+    let sidebar = document.querySelector("#sidebar");
+    sidebar.classList.toggle("active");
     document.getElementById("overlay").style.display = "none";
 }
 
 // Card Pop up effect
 function openCardPopUp() {
     console.log(this);
-    let card = document.querySelector('.card');
-    document.querySelector('.card').lastElementChild.style.top = '0';
-    document.querySelector('.card').lastElementChild.style.cursor = 'auto';
+    let card = document.querySelector(".card");
+    document.querySelector(".card").lastElementChild.style.top = "0";
+    document.querySelector(".card").lastElementChild.style.cursor = "auto";
 }
 
 function closeCardPopUp() {
-    let card = document.querySelector('.card');
-    document.querySelector('.card').lastElementChild.style.top = '400px'
+    let card = document.querySelector(".card");
+    document.querySelector(".card").lastElementChild.style.top = "400px";
 }
-
 
 // Smooth Scrolling
 const scroll = new SmoothScroll('#sidebar a[href*="#"], #navbar a[href*="#"]', {
-    speed: 500
+    speed: 500,
 });
